0bf7ec502535219c1848f19696d139858b0ab93c
Nosa Igbinoba <65290746+NosaIgbinoba@users.noreply.github.com>
2023-12-14 23:21:30 +0000

    Update mongodb.js

--- ExploreEpicProject/Travel/src/mongodb.js
+++ ExploreEpicProject/Travel/src/mongodb.js
@@ -1,31 +1,42 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 
+// Replace this with your Azure Cosmos DB connection string
 const connectionString = "mongodb://exploreepicdatabase:80l4sRze5uQpbqlRCGdT7d5htDwiOadyVok0djJknNt4TFIu0occtM9dTbSzgMiQFQFDw8HnwZyWACDbj7bIXQ==@exploreepicdatabase.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@exploreepicdatabase@";
 
-// Function to connect to Cosmos DB and export the collection for use in other parts of the application
-async function connectAndExportCollection() {
+async function connectToDatabase() {
   try {
-    const client = new MongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-    await client.connect();
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to Azure Cosmos DB");
 
-    console.log('Connected successfully to Cosmos DB');
+    // Define a schema for the "Collection1" documents
+    const LogInSchema = new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      posts: {
+        type: Array,
+        required: false,
+        default: [],
+      },
+    });
 
-    // Access the "Collection1" collection from Azure Cosmos DB using the 'client' object
-    const db = client.db('exploreepicdatabase');
-    const collection = db.collection('Collection1');
+    // Create a mongoose model based on the schema, named "Collection1"
+    const collection = mongoose.model("Collection1", LogInSchema);
 
-    // Export the "Collection1" collection for use in other parts of the application
-    module.exports = collection;
-
-    // You can perform operations using the 'collection' here if needed
-    // For example, find documents, insert, update, or delete documents
-
-    // Close the client/connection when done (or manage it as per your application's flow)
-    // await client.close();
+    return collection; // Return the model to use in other parts of the application
   } catch (error) {
-    console.error('Failed to connect:', error);
+    console.error("Error connecting to Azure Cosmos DB:", error);
+    throw error; // Throw an error if the connection fails
   }
 }
 
-// Call the function to connect and export the collection
-connectAndExportCollection();
+module.exports = connectToDatabase; // Export the function to connect to the database
